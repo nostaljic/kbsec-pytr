72939acc2d5f879a3f0b248bc8df00471256755c
kbsec-pytr-dev <dev@kbsec-pytr>
2026-05-22 05:17:28 +0000

    fix: align exception class names; fix tr_rule XML attribute parsing for jkass-config format

--- kbsec/exceptions.py
+++ kbsec/exceptions.py
@@ -1,8 +1,14 @@
 class KBSecConfigError(Exception):
     pass
 
-class KBSecConnectionError(Exception):
+class KBConnectionError(Exception):
+    pass
+
+class KBTimeoutError(Exception):
     pass
 
 class KBSecRequestError(Exception):
     pass
+
+# Aliases for backward compat
+KBSecConnectionError = KBConnectionError
--- kbsec/tr_rule.py
+++ kbsec/tr_rule.py
@@ -25,25 +25,38 @@ class TrRuleManager:
 
     @classmethod
     def load(cls, rule_dir: str) -> None:
+        """Load TR rule XMLs from directory.
+
+        XML structure: <Name>TR_CODE</Name>
+        Input fields:  <I Name="..." Size="..." format="..." Desc="..." />
+        Output fields: <O Name="..." Size="..." format="..." Desc="..." />
+        """
         for xml_file in Path(rule_dir).glob('*.xml'):
             try:
                 tree = ET.parse(xml_file)
                 root = tree.getroot()
-                tr_code = root.findtext('TrCode') or xml_file.stem
+                # <Name>GSS10030</Name>
+                tr_code = root.findtext('Name') or xml_file.stem
                 rule = TrRule(tr_code=tr_code)
-                for f in root.findall('.//Input/Field'):
+                # Input fields use <I Name="..." Size="..." /> attributes
+                for f in root.findall('.//Input/I'):
+                    name = f.get('Name') or ''
+                    size_str = f.get('Size') or '0'
                     rule.input_fields.append(FieldSpec(
-                        name=f.findtext('Name') or '',
-                        size=int(f.findtext('Size') or 0),
-                        type=f.findtext('Type') or 'A',
-                        desc=f.findtext('Desc') or '',
+                        name=name,
+                        size=int(size_str),
+                        type=f.get('format') or 'A',
+                        desc=f.get('Desc') or '',
                     ))
-                for f in root.findall('.//Output/Field'):
+                # Output fields use <O Name="..." Size="..." /> attributes
+                for f in root.findall('.//Output/O'):
+                    name = f.get('Name') or ''
+                    size_str = f.get('Size') or '0'
                     rule.output_fields.append(FieldSpec(
-                        name=f.findtext('Name') or '',
-                        size=int(f.findtext('Size') or 0),
-                        type=f.findtext('Type') or 'A',
-                        desc=f.findtext('Desc') or '',
+                        name=name,
+                        size=int(size_str),
+                        type=f.get('format') or 'A',
+                        desc=f.get('Desc') or '',
                     ))
                 cls._rules[tr_code] = rule
             except Exception:
